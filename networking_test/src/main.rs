4abbe02d89918cc4801ea8170f18f6e6bff971c8
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2024-01-28 15:42:36 +1100

    Fix HTTP Server logging

--- networking_test/src/main.rs
+++ networking_test/src/main.rs
@@ -3,35 +3,44 @@ use axum::{
     response::Html,
     Router,
 };
+use std::sync::{Arc, Mutex};
 
 #[tokio::main]
 async fn main() {
-    let mut request_count: u32 = 0;
+    let request_count = Arc::new(Mutex::new(0));
 
     let app = Router::new()
-        .route("/", get(move || {
-            println!("home");
-            let request_count = request_count;
-            async move {
-                Html(format!(
-                    r#"<h1>Welcome to TPUv6/TPUvRust</h1>
-                    <p>Way better than TPUv5</p>
-                    <p>Request count: {}</p>
-                    <form action="/request" method="post">
-                        <button name="foo" value="upvote">Upvote</button>
-                    </form>"#,
-                request_count
-            ))
-        }
+        .route("/", get({
+            let request_count = Arc::clone(&request_count);
+            move || {
+                async move {
+                    let mut request_count = request_count.lock().unwrap();
+                    *request_count += 1;
+                    Html(format!(
+                        r#"<h1>Welcome to TPUv6/TPUvRust</h1>
+                        <p>Way better than TPUv5</p>
+                        <p>Request count: {}</p>
+                        <form action="/request" method="post">
+                            <button name="foo" value="upvote">Upvote</button>
+                        </form>"#,
+                        *request_count
+                    ))
+                }
+            }
         }))
-        .route("/request", post(move || {
-            request_count += 1;
-            println!("request {}", request_count);
-            async move {
-                format!("{}", request_count);
+        .route("/request", post({
+            let request_count = Arc::clone(&request_count);
+            move || {
+                let request_count = Arc::clone(&request_count);
+                async move {
+                    let mut request_count = request_count.lock().unwrap();
+                    *request_count += 1;
+                    Html(format!("{}", *request_count))
+                }
             }
         }));
-    
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+
