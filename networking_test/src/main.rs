9848c77487c33729ed15458f17962db61684cfdf
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2024-03-28 11:39:04 +1100

    Dotenv and SQLx MySQL + Updated deps

--- networking_test/src/main.rs
+++ networking_test/src/main.rs
@@ -1,19 +1,36 @@
 use axum::{
-    routing::{get, post},
     response::Html,
+    routing::{get, post},
     Router,
 };
+use dotenv::dotenv;
+use sqlx::mysql::MySqlPoolOptions;
 use std::sync::{Arc, Mutex};
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(&(database_url + ":password@localhost/test"))
+        .await;
+
+    /* let mut email = "Email";
+
+    let mut rows = sqlx::query("SELECT * FROM Users WHERE email = ?")
+        .bind(email)
+        .fetch(&mut pool); */
+
     let request_count = Arc::new(Mutex::new(0));
 
     let app = Router::new()
-        .route("/", get({
-            let request_count = Arc::clone(&request_count);
-            move || {
-                async move {
+        .route(
+            "/",
+            get({
+                let request_count = Arc::clone(&request_count);
+                move || async move {
                     let mut request_count = request_count.lock().unwrap();
                     *request_count += 1;
                     Html(format!(
@@ -26,21 +43,23 @@ async fn main() {
                         *request_count
                     ))
                 }
-            }
-        }))
-        .route("/request", post({
-            let request_count = Arc::clone(&request_count);
-            move || {
+            }),
+        )
+        .route(
+            "/request",
+            post({
                 let request_count = Arc::clone(&request_count);
-                async move {
-                    let mut request_count = request_count.lock().unwrap();
-                    *request_count += 1;
-                    Html(format!("{}", *request_count))
+                move || {
+                    let request_count = Arc::clone(&request_count);
+                    async move {
+                        let mut request_count = request_count.lock().unwrap();
+                        *request_count += 1;
+                        Html(format!("{}", *request_count))
+                    }
                 }
-            }
-        }));
+            }),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
-
